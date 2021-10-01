08828ed5c753e990f8c4e3aaef5e8be50ec4e167
David Le <davidle90@hotmail.com>
2021-10-01 13:48:53 +0200

    add menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+using System.Threading;
 
 namespace GitTODO
 {
@@ -6,7 +8,57 @@ namespace GitTODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool running = true;            //Run application while true
+
+            while (running)
+            {
+                Clear();
+                WriteLine("\tMain Menu");
+                WriteLine("\t[1]Add Product");
+                WriteLine("\t[2]Search Product");
+                WriteLine("\t[3]Show All Products");
+                WriteLine("\t[4]Exit");
+
+                var input = ReadKey();
+
+                switch (input.Key)
+                {
+
+                    case ConsoleKey.D1:              //Menu 1: Add Product
+                    case ConsoleKey.NumPad1:
+
+                        WriteLine("Hello World");
+                        ReadKey();
+                        break;
+
+                    case ConsoleKey.D2:             //Menu 2: Search Product
+                    case ConsoleKey.NumPad2:
+
+                        WriteLine("Goodbye WOrld");
+                        ReadKey();
+                        break;
+
+                    case ConsoleKey.D3:              //Menu 3: Show All Products
+                    case ConsoleKey.NumPad3:
+
+                        WriteLine("ok world");
+                        ReadKey();
+                        break;
+
+                    case ConsoleKey.D4:              //Menu 3: Exit
+                    case ConsoleKey.NumPad4:
+
+                        Clear();
+                        WriteLine("Closing application..");
+                        Thread.Sleep(1000);
+                        running = false;
+                        break;
+
+                    default:                        //Nothing happens on invalid input
+                        break;
+                }
+            }
+
         }
     }
 }
