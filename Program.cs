e17f88370ae7b36c4b78ed0f716dd9d3647dfa35
David Le <davidle90@hotmail.com>
2021-10-01 11:08:02 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTODO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
